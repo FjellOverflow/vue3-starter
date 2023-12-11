f9888e013b2a616abf50b5ed039bcac30b00882d
FjellOverflow <fjelloverflow@protonmail.com>
2023-12-11 21:21:06 +0200

    test: add i18n to mounted component in test

--- test/HelloWorld.spec.ts
+++ test/HelloWorld.spec.ts
@@ -2,10 +2,13 @@ import { describe, expect, it } from 'vitest'
 
 import { mount } from '@vue/test-utils'
 import HelloWorld from '../src/components/HelloWorld.vue'
+import { i18n } from '../src/i18n'
 
 describe('renders HelloWorld', () => {
   it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello, World!' } })
+    const wrapper = mount(HelloWorld, { props: { msg: 'Hello, World!' }, global: {
+      plugins: [i18n],
+    } })
     expect(wrapper.text()).toContain('Hello, World!')
   })
 })
