2c92a7c87448f52ca050825c5ffc1e830c0d8e91
FjellOverflow <fjelloverflow@protonmail.com>
2024-11-21 22:50:49 +0200

    refactor: cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5173',
+    specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,ts}',
+    baseUrl: 'http://localhost:4173',
   },
 })
