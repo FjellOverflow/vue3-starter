6a288e97d7badadbdf2baf3251f7121d2989c5f6
FjellOverflow <fjelloverflow@protonmail.com>
2024-11-21 22:36:43 +0200

    refactor: import routes from `vue-router/auto-routes`

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
+
 import App from './App.vue'
-import { router } from './router'
+import router from './router'
 import { i18n } from './i18n'
 
 const app = createApp(App)
--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router/auto'
+import { createRouter, createWebHistory } from 'vue-router'
+import { routes } from 'vue-router/auto-routes'
 import { setupLayouts } from 'virtual:generated-layouts'
 
-export const router = createRouter({
-  history: createWebHistory(),
-  extendRoutes: routes => setupLayouts(routes),
-
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: setupLayouts(routes),
 })
+
+export default router
