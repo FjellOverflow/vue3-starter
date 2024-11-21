2aaed1f4dab25e93b45eb7c5d3b3ddb997db8782
FjellOverflow <fjelloverflow@protonmail.com>
2024-11-21 22:55:13 +0200

    refactor: tsconfigs

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,16 @@
-import { defineConfig } from 'vitest/config'
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueRouter from 'unplugin-vue-router/vite'
-import components from 'unplugin-vue-components/vite'
-import layouts from 'vite-plugin-vue-layouts'
+import vueLayouts from 'vite-plugin-vue-layouts'
+import vueComponents from 'unplugin-vue-components/vite'
 
 export default defineConfig({
-  plugins: [
-    vueRouter(),
-    vue(),
-    components(),
-    layouts(),
-  ],
-  test: {
-    environment: 'jsdom',
+  plugins: [vueRouter(), vue(), vueLayouts(), vueComponents()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
   },
 })
