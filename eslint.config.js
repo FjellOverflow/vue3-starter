f220eb7aa0a40d597e2d0fa80a6ae9f779325522
FjellOverflow <fjelloverflow@protonmail.com>
2024-11-21 22:57:45 +0200

    chore: - drop `antfu` eslint config - switch to prettier - bump deps - new package scripts

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,36 @@
-import antfu from '@antfu/eslint-config'
+import pluginVue from 'eslint-plugin-vue'
+import vueTsEslintConfig from '@vue/eslint-config-typescript'
+import pluginVitest from '@vitest/eslint-plugin'
+import pluginCypress from 'eslint-plugin-cypress/flat'
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
-export default antfu()
+export default [
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{ts,mts,tsx,vue}'],
+  },
+
+  {
+    name: 'app/files-to-ignore',
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+  },
+
+  ...pluginVue.configs['flat/essential'],
+  ...vueTsEslintConfig(),
+  {
+    rules: {
+      'vue/multi-word-component-names': 'off',
+    },
+  },
+
+  {
+    ...pluginVitest.configs.recommended,
+    files: ['src/**/__tests__/*'],
+  },
+
+  {
+    ...pluginCypress.configs.recommended,
+    files: ['cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}', 'cypress/support/**/*.{js,ts,jsx,tsx}'],
+  },
+  skipFormatting,
+]
