56687e7d2ee23581429ec790220576a26228e110
FjellOverflow <fjelloverflow@protonmail.com>
2025-12-20 14:57:34 +0100

    chore: replace logo & theme colors

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -5,7 +5,7 @@ const route = useRoute()
 <template>
   <div>
     <a href="https://github.com/FjellOverflow/vue3-starter" target="_blank">
-      <img src="@/assets/logo.svg" class="logo" alt="The Vue logo in gray/blue color" />
+      <img src="@/assets/logo.svg" class="logo" alt="The Vue logo" />
     </a>
   </div>
   <h1>{{ route.meta.title }}</h1>
@@ -29,6 +29,6 @@ const route = useRoute()
   transition: filter 300ms;
 }
 .logo:hover {
-  filter: drop-shadow(0 0 2em #81a1c1);
+  filter: drop-shadow(0 0 2em #00bd7e);
 }
 </style>
